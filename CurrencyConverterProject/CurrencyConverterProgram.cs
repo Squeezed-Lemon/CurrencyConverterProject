eac97a3df6b37bf4711496e58d037543bd6b70a3
Scrippt <vasconcelos.aba@gmail.com>
2026-03-17 15:17:38 +0100

    Creating variables for each symbol corresponding to each currency and creating a condition for the dollar input.

--- CurrencyConverterProject/CurrencyConverterProgram.cs
+++ CurrencyConverterProject/CurrencyConverterProgram.cs
@@ -12,10 +12,35 @@
             yuan_input = "yuan";
             real_input = "real";
 
+            //Criando sinais referente a cada currency
+            string dollar_symb, euro_symb, yuan_symb, real_symb;
+
+            dollar_symb ="$";
+            euro_symb = "€";
+            yuan_symb = "¥";
+            real_symb = "R$";
+
             
             //Creating the question1
             Console.WriteLine("Which currency would you like to convert? \n \n --Dollar (USD)-- \n --Euro (EUR)-- \n --Yuan (CNY)-- \n --Real (BRL)-- \n");
             string type_initial_user_currency = Console.ReadLine(); //Creating a variable to store the user input.
+
+
+            //Criando a condicao para caso o usuario tenha escolhido a moeda dolar
+            if (type_initial_user_currency == dollar_input) 
+            {
+                //Question2 Dollar
+                Console.WriteLine("\nWhat amount would you like to convert?");
+
+                Console.Write(dollar_symb); //Dolar sign for response block2
+                decimal value_type_initial_user_currency = decimal.Parse(Console.ReadLine()); //Input response block 2
+
+
+
+
+
+
+            }
 
 
 
