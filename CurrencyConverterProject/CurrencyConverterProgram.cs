702fe7627ac2a46107645827a42c6ea1b1b49e7e
Scrippt <vasconcelos.aba@gmail.com>
2026-03-17 14:26:00 +0100

    Creating a variable for each currency

--- CurrencyConverterProject/CurrencyConverterProgram.cs
+++ CurrencyConverterProject/CurrencyConverterProgram.cs
@@ -4,6 +4,21 @@
     {
         public static void Main() 
         {
+            //Creating a variable for each currency.
+            string dolar_input, euro_input, yuan_input, real_input;
+
+            dolar_input = "dolar";
+            euro_input = "euro";
+            yuan_input = "yuan";
+            real_input = "real";
+
+
+
+
+
+
+
+
 
         }
     }
