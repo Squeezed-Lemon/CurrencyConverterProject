b7e51880b6c84bc79529beb5c09c8a0f67f68513
Scrippt <vasconcelos.aba@gmail.com>
2026-03-17 14:52:22 +0100

    Creating a variable to store the user input and creating the question1

--- CurrencyConverterProject/CurrencyConverterProgram.cs
+++ CurrencyConverterProject/CurrencyConverterProgram.cs
@@ -1,16 +1,23 @@
 ﻿namespace CurrencyConverter 
 {
-    public class Program 
+    public class Program
     {
-        public static void Main() 
+        public static void Main()
         {
             //Creating a variable for each currency.
-            string dolar_input, euro_input, yuan_input, real_input;
+            string dollar_input, euro_input, yuan_input, real_input;
 
-            dolar_input = "dolar";
+            dollar_input = "dollar";
             euro_input = "euro";
             yuan_input = "yuan";
             real_input = "real";
+
+            
+            //Creating the question1
+            Console.WriteLine("Which currency would you like to convert? \n \n --Dollar (USD)-- \n --Euro (EUR)-- \n --Yuan (CNY)-- \n --Real (BRL)-- \n");
+            string type_initial_user_currency = Console.ReadLine(); //Creating a variable to store the user input.
+
+
 
 
 
